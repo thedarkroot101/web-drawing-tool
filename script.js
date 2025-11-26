8eae4fdabbe397316d95ef7dd20dd7a6c5d559a2
thedarkroot101 <thedarkroot101@gmail.com>
2025-11-26 16:19:47 +0530

    Add interactive SVG circle drawing tool
    Replaces the SVG element id from 'circle' to 'canvas' in index.html and implements interactive circle drawing on mouse events in script.js. Adds basic styles for layout and SVG canvas in styles.css. Users can now draw circles by clicking and dragging on the SVG canvas.

--- script.js
+++ script.js
@@ -1,2 +1,46 @@
-const mapArea = document.getElementById("mapArea");
-const circle = document.querySelector("#circle");
+const canvas = document.querySelector("#canvas");
+
+let temp = null;
+
+const state = {
+    startX: 0,
+    startY: 0,
+    toolType: null, // line, circle, etc
+    color: '#000000ff',
+    fillColor: '#00000001',
+    width: 2,
+};
+
+canvas.addEventListener('mousedown', (e) => {
+    state.toolType = 'circle';
+    state.startX = e.clientX;
+    state.startY = e.clientY;
+
+    const circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+    circle.setAttribute("cx", state.startX);
+    circle.setAttribute("cy", state.startY);
+    circle.setAttribute("r", 0);
+    circle.setAttribute("stroke", state.color);
+    circle.setAttribute("fill", state.fillColor);
+    circle.setAttribute("stroke-width", state.width);
+
+    canvas.appendChild(circle);
+
+    temp = circle;
+});
+
+canvas.addEventListener('mousemove', (e) => {
+
+    if (state.toolType === 'circle') {
+        const a = e.clientX - state.startX;
+        const b = e.clientY - state.startY;
+        const radius = Math.sqrt(a * a + b * b);
+
+        temp.setAttribute('r', radius);
+    }
+});
+
+canvas.addEventListener('mouseup', () => {
+    state.toolType = null;
+    temp = null;
+});
